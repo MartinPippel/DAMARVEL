20fe29801215f0b23ff919420dc8a6a095ae8ead
pippel <pippel@mpi-cbg.de>
2019-11-28 17:33:10 +0100

    bugfix

--- scripts/DAmarReadPatchingPipeline.sh
+++ scripts/DAmarReadPatchingPipeline.sh
@@ -486,8 +486,22 @@ then
             	echo "cd ${DALIGN_OUTDIR} && ${MARVEL_PATH}/bin/LArepeat${LAREPEAT_OPT} -l${REPEAT_LOWCOV[${y}]} -h${REPEAT_HGHCOV[${y}]} -c${REPEAT_COV[${y}]} -t${REPEAT_TRACK[${y}]} -b ${x} ${DB_M%.db} ${DB_Z%.db}.dalignFilt.${x}.las && cd ${myCWD}/"
             	if [[ ${REPEAT_COV[${y}]} -gt 0 ]]
             	then
-            		repmaskCov=$(echo "${REPEAT_HGHCOV[${y}]} ${REPEAT_COV[${y}]}" | awk '{printf "%d", $1*$2}')
-            		echo "cd ${DALIGN_OUTDIR} && ${DAZZLER_PATH}/bin/REPmask -v -c${repmaskCov} -n${REPEAT_TRACK[$y]} ${DB_Z%.db} ${DB_Z%.db}.dalignFilt.${x}.las && cd ${myCWD}/"
+            		found=0
+            		z=0
+            		while [[ $z -lt $y ]]
+            		do
+            			if [[ ${REPEAT_COV[${z}]} == ${REPEAT_COV[${y}]} ]]
+            			then
+            				found=1
+            				break
+            			fi
+            			z=$((z+1))
+            		done
+            		if [[ ${found} -eq 0 ]]
+            		then
+            			repmaskCov=$(echo "${REPEAT_HGHCOV[${y}]} ${REPEAT_COV[${y}]}" | awk '{printf "%d", $1*$2}')
+            			echo "cd ${DALIGN_OUTDIR} && ${DAZZLER_PATH}/bin/REPmask -v -c${repmaskCov} -n${REPEAT_TRACK[$y]} ${DB_Z%.db} ${DB_Z%.db}.dalignFilt.${x}.las && cd ${myCWD}/"
+            		fi
             	fi
             	y=$((y+1))
         	done
