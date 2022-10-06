b5f3a8d90eab98131381111aebd77023c61dcd28
Martin Pippel <pippel@myers-mac-30.local>
2022-10-06 15:06:03 +0200

    ONT polishing add step 9 and 10 - bugfix LArepeat

--- scripts/createCorrectionPlans.sh
+++ scripts/createCorrectionPlans.sh
@@ -1123,7 +1123,7 @@ then
     		do
         		echo "cd ${Daccord_DIR} && ${MARVEL_PATH}/bin/LArepeat${DACCORD_LAREPEAT_OPT[$x]} -b ${y} ${DACCORD_DB%.db} ${DACCORD_DB%.db}.dalignFilt.${y}.las && cd ${myCWD}/"            		
     		done
-    		echo "cd ${Daccord_DIR} && ${DAZZLER_PATH}/bin/REPmask${DACCORD_DAZZ_LAREPEAT_OPT} ${DACCORD_DAZZ_DB%.db} ${DACCORD_DAZZ_DB%.db}.dalignFilt.${y}.las && cd ${myCWD}/"
+    		echo "cd ${Daccord_DIR} && ${DAZZLER_PATH}/bin/REPmask${DACCORD_DAZZ_LAREPEAT_OPT} ${DACCORD_DAZZ_DB%.db} ${DACCORD_DB%.db}.dalignFilt.${y}.las && cd ${myCWD}/"
     	done > corr_09_LArepeat_block_${FIX_DB%.db}.${slurmID}.plan 
         echo "MARVEL LArepeat $(git --git-dir=${MARVEL_SOURCE_PATH}/.git rev-parse --short HEAD)" >corr_09_LArepeat_block_${FIX_DB%.db}.${slurmID}.version
         echo "DAZZLER REPmask $(git --git-dir=${DAZZLER_SOURCE_PATH}/DAMASKER/.git rev-parse --short HEAD)" >> corr_09_LArepeat_block_${FIX_DB%.db}.${slurmID}.version         
