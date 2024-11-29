3bb9f33661543d594847fd9e92c1ed90652e527b
MartinPippel <martin.pippel@nbis.se>
2024-11-29 13:44:10 +0100

    bugfix typo

--- scripts/createHiCPlans.sh
+++ scripts/createHiCPlans.sh
@@ -1166,7 +1166,7 @@ then
         
         for i in "${SC_HIC_HIGLASS_COOLERRESOLUTION[@]}"
 		do
-			echo -n "bgzip -c -${BGZIP_THREADS} ${SC_HIC_OUTDIR}/hic_${SC_HIC_RUNID}/filter/${PROJECT_ID}_allHiC.filtered.pairs.gz | "			
+			echo -n "bgzip -c -@${BGZIP_THREADS} ${SC_HIC_OUTDIR}/hic_${SC_HIC_RUNID}/filter/${PROJECT_ID}_allHiC.filtered.pairs.gz | "			
 			echo -n "HDF5_USE_FILE_LOCKING=FALSE cooler cload pairix -p ${SC_HIC_HIGLASS_PAIRTOOLSTHREADS} ${SC_HIC_OUTDIR}/hic_${SC_HIC_RUNID}/ref/$(basename ${SC_HIC_REF%.fasta}).chrom.sizes:${i} - ${SC_HIC_OUTDIR}/hic_${SC_HIC_RUNID}/matrix/${PROJECT_ID}_allHiC.output.${i}.cool"   	
 			# normalization - (matrix balancing)
 			echo -n " && HDF5_USE_FILE_LOCKING=FALSE cooler balance -p ${SC_HIC_HIGLASS_PAIRTOOLSTHREADS} ${SC_HIC_OUTDIR}/hic_${SC_HIC_RUNID}/matrix/${PROJECT_ID}_allHiC.output.${i}.cool"
